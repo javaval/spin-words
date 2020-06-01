de288b4a167766864ca84c33ec613613138c1cbe
Valerie Andy <valdaraandy@gmail.com>
2020-06-01 01:06:56 -0400

    update

--- src/main/java/src/Solution.java
+++ src/main/java/src/Solution.java
@@ -4,7 +4,7 @@ public class Solution {
 
     public String spinWords(String sentence) {
       //TODO: Code stuff here
-      String str = sentence;
-      return str;
+
+      return sentence;
     }
   }
