5525519a885a7b93214b79fd4bdcd6c2b542e146
Valerie Andy <valdaraandy@gmail.com>
2020-06-01 00:34:35 -0400

    Initial Commit

--- src/main/java/src/Solution.java
+++ src/main/java/src/Solution.java
@@ -4,6 +4,7 @@ public class Solution {
 
     public String spinWords(String sentence) {
       //TODO: Code stuff here
-      return sentence;
+      String str = sentence;
+      return str;
     }
   }
